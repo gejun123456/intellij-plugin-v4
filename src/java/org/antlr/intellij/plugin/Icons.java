874dfa157e0d0718511826304a3a95f9587c8c1e
葛俊 <gejun123456@gmail.com>
2018-09-26 11:07:36 +0800

    fix for icons in gradle project.

--- src/java/org/antlr/intellij/plugin/Icons.java
+++ src/java/org/antlr/intellij/plugin/Icons.java
@@ -5,7 +5,7 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public class Icons {
-	public static final Icon FILE = IconLoader.getIcon("/org/antlr/intellij/plugin/antlr.png");
-	public static final Icon LEXER_RULE = IconLoader.getIcon("/org/antlr/intellij/plugin/lexer-rule.png");
-	public static final Icon PARSER_RULE = IconLoader.getIcon("/org/antlr/intellij/plugin/parser-rule.png");
+	public static final Icon FILE = IconLoader.getIcon("/icons/org/antlr/intellij/plugin/antlr.png");
+	public static final Icon LEXER_RULE = IconLoader.getIcon("/icons/org/antlr/intellij/plugin/lexer-rule.png");
+	public static final Icon PARSER_RULE = IconLoader.getIcon("/icons/org/antlr/intellij/plugin/parser-rule.png");
 }
